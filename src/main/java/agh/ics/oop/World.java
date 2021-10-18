8b578e148fda1a437088485ad8af03570d0d3c07
Kamil Bizoń <matbiz01@gmail.com>
2021-10-18 18:28:36 +0200

    lab1 zrobione
    Nie jestem pewien, czy wypisywanie argumentów po przecinku jest dalej konieczne

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,7 +1,58 @@
 package agh.ics.oop;
+import static java.lang.System.out;
+
+enum Direction {
+    FORWARD,
+    BACKWARD,
+    RIGHT,
+    LEFT,
+    WRONG_INPUT
+}
 
 public class World {
     public static void main(String[] args) {
-        System.out.println("aaa");
+        out.println("start");
+
+        Direction[] directions = StringToEnum(args);
+        run(directions);
+
+        out.println("stop");
+    }
+
+    private static void run(Direction[] directions){
+        int dirCount = directions.length;
+        for(int i = 0; i < dirCount; i++){
+            switch(directions[i]){
+                case FORWARD:
+                    out.println("forward");
+                    break;
+                case BACKWARD:
+                    out.println("backward");
+                    break;
+                case LEFT:
+                    out.println("left");
+                    break;
+                case RIGHT:
+                    out.println("right");
+                    break;
+                case WRONG_INPUT:
+                    out.println("wrong input");
+            }
+        }
+    }
+
+    private static Direction[] StringToEnum(String[] stringDirections){
+        int dirCount = stringDirections.length;
+        Direction[] enumDirections = new Direction[dirCount];
+        for(int i = 0; i < dirCount; i++){
+            enumDirections[i] = switch(stringDirections[i]){
+                case "f" -> Direction.FORWARD;
+                case "b" -> Direction.BACKWARD;
+                case "l" -> Direction.LEFT;
+                case "r" -> Direction.RIGHT;
+                default -> Direction.WRONG_INPUT;
+            };
+        }
+        return enumDirections;
     }
 }
