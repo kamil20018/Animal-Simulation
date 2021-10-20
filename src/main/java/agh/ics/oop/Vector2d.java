8c7d57ff1176b312f0aa6767d42873ecf8517da1
Kamil Bizoń <matbiz01@gmail.com>
2021-10-20 18:35:29 +0200

    lab2
    lab2 zrobione

---
+++ src/main/java/agh/ics/oop/Vector2d.java
@@ -0,0 +1,59 @@
+package agh.ics.oop;
+import static java.lang.System.out;
+
+public class Vector2d {
+    public final int x;
+    public final int y;
+
+    public Vector2d(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString(){
+        return "(" + x + ", " + y + ")";
+    }
+
+    public boolean precedes(Vector2d other){
+        return this.x <= other.x && this.y <= other.y;
+    }
+
+    public boolean follows(Vector2d other){
+        return this.x >= other.x && this.y >= other.y;
+    }
+
+    public Vector2d upperRight(Vector2d other){
+        int biggerX = Math.max(this.x, other.x);
+        int biggerY = Math.max(this.y, other.y);
+
+        return new Vector2d(biggerX, biggerY);
+    }
+
+    public Vector2d lowerLeft(Vector2d other){
+        int lowerX = Math.min(this.x, other.x);
+        int lowerY = Math.min(this.y, other.y);
+
+        return new Vector2d(lowerX, lowerY);
+    }
+
+    public Vector2d add(Vector2d other){
+        return new Vector2d(this.x + other.x, this.y + other.y);
+    }
+
+    public Vector2d subtract(Vector2d other){
+        return new Vector2d(this.x - other.x, this.y - other.y);
+    }
+
+    public Vector2d opposite(){
+        return new Vector2d(-this.x, -this.y);
+    }
+
+    public boolean equals(Object other){
+        if (this == other)
+            return true;
+        if (!(other instanceof Vector2d))
+            return false;
+        Vector2d that = (Vector2d) other;
+        return this.x == that.x && this.y == that.y;
+    }
+}
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,22 +1,21 @@
 package agh.ics.oop;
 import static java.lang.System.out;
 
-enum Direction {
-    FORWARD,
-    BACKWARD,
-    RIGHT,
-    LEFT,
-    WRONG_INPUT
-}
+
 
 public class World {
     public static void main(String[] args) {
         out.println("start");
 
-        Direction[] directions = StringToEnum(args);
+        Direction[] directions = stringToEnum(args);
         run(directions);
-
         out.println("stop");
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 
     private static void run(Direction[] directions){
@@ -35,13 +34,11 @@ public class World {
                 case RIGHT:
                     out.println("right");
                     break;
-                case WRONG_INPUT:
-                    out.println("wrong input");
             }
         }
     }
 
-    private static Direction[] StringToEnum(String[] stringDirections){
+    private static Direction[] stringToEnum(String[] stringDirections){
         int dirCount = stringDirections.length;
         Direction[] enumDirections = new Direction[dirCount];
         for(int i = 0; i < dirCount; i++){
@@ -50,7 +47,7 @@ public class World {
                 case "b" -> Direction.BACKWARD;
                 case "l" -> Direction.LEFT;
                 case "r" -> Direction.RIGHT;
-                default -> Direction.WRONG_INPUT;
+                default -> null;
             };
         }
         return enumDirections;
